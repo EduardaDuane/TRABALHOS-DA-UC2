2c059e1714d6fb61ec15bd4b8ad68bc825261214
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-04-01 17:05:04 -0300

    atualizando dnv...

--- prompt/LiveServer/exercicio1.js
+++ prompt/LiveServer/exercicio1.js
@@ -3,10 +3,11 @@ let poder2 = prompt ("Escolher seu segundo poder para combinar:")
 
 let cristais1= prompt ("Quantos cristais vai usar para ativar o poder " + poder1 + "?")
 let cristais2= prompt ("Quantos cristais vai usar para ativar o poder " + poder2 + "?")
+
 cristais1 = Number(cristais1)
 cristais2 = Number (cristais2)
 
-let totalCristais = cristais1 = cristais2
+let totalCristais = cristais1 + cristais2
 
 
 alert("Você combinou os poderes " + poder1 + " e " + poder2 + " usando " + totalCristais + " cristais mágicos no total. ")
