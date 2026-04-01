e6503c02be9799454f2df7fa9dddc01f8defbb6a
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-04-01 15:16:48 -0300

    subindo arquivos

---
+++ exercicios.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')()
+
+/*let cliente= prompt ("Qual o seu nome?")
+let sabor= prompt ("Qual o sabor da pizza?")
+let tamanho= prompt ("Qual o tamanho da pizza?")
+
+console.log(`Seja bem vindo ${cliente}. Seu pedido é uma pizza de ${sabor}, no tamanho ${tamanho}. Já está no forno!`)*/
+
+/*let personagem= prompt ("Qual o seu nome?")
+let profissao= prompt ("Qual o sua profissão?")
+let obj= prompt ("Qual o seu objeto ou arma?")
+
+console.log(`O meu personagem é ${personagem}, sua profissão é ${profissao} e para ele salvar o mundo ele usa um(a) ${obj}!`)*/
+
+let nome= prompt ("Qual o seu nome?")
+let idade= prompt ("Qual o seu idade?")
+let fazer= prompt ("O que você gosta de fazer?")
+let anime= prompt ("Qual o seu anime favorito?")
+let serie= prompt ("Qual o sua série favorita?")
+let curiosidade= prompt ("Me conte uma curiosidade sua:")
+let time= prompt ("Qual o seu time do coração?")
+
+console.log(`O meu nome é ${nome}🩵, tenho ${idade}🎂, gosto de ${fazer} no meu tempo livre💤, eu ${anime}❌, minha série favorita é ${serie}📺, uma curiosidade sobre mim é que eu ${curiosidade}🎹 e para finalizar, o meu time é ${time}!❤️`)
