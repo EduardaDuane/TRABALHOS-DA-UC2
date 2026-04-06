7de93a6b16d756cd7ac75a836e1dc362c76101a7
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-04-06 16:40:28 -0300

    subindo arquivos de hoje

---
+++ prompt/promptSync_/bullying.js
@@ -0,0 +1,14 @@
+
+const prompt = require('prompt-sync')()
+
+let bullyng = prompt ("O que é bullyng?")
+let assimetria= prompt ("O que é assimetria de poder?")
+let defina= prompt ("Defina Cyberbullyng:")
+let Interseccionalidade= prompt ("O que é Interseccionalidade?")
+let relação= prompt ("Qual a relação com o bullyng?")
+let Kimberlé= prompt ("Quem foi Kimberlé Crenshaw?")
+let preconceitos= prompt ("O que são preconceitos estruturais? Explique e citre 3 deles.")
+let ocorre= prompt ("O bullying ocorre por acaso?")
+let crime= prompt ("Bullying é crime?")
+
+console.log(`O bullying é ${bullyng} Além disso, a assimetria de poder é descrita como ${assimetria}. No mundo digital, temos o ${defina}. Tudo isso se cruza com a ${Interseccionalidade}, que tem ${relação}. Esse conceito foi estudado por ${Kimberlé}, que nos ajuda a entender que ${preconceitos}. No fim, entendemos que o bullying não ${ocorre} e, legalmente, ${crime}.`);
