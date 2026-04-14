c8fc2396c96459e9542919252cbcd3c9be72e00f
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-04-14 17:18:25 -0300

    subindo arquivos

---
+++ funcoes/exercicios.js
@@ -0,0 +1,5 @@
+function imprimirTDS(){
+
+console.log("TDS")}
+
+imprimirTDS()
---
+++ funcoes/tiposFuncoes.js
@@ -0,0 +1,11 @@
+function imprimirOlaMundo(){
+    console.log("Olá mundo!!!")
+}
+imprimirOlaMundo()
+
+function somar(a,b){
+    return a + b
+}
+
+let resultado = somar(5,3)
+console.log(resultado)
